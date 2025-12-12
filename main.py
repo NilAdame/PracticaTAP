e17c9fe962134e92eaa087a90ef5fc4af6490430
Nil Adame <nil.adame@estudiants.urv.cat>
2025-12-12 16:42:44 +0100

    main, conexion con el chat de minecraft

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from mcpi.minecraft import Minecraft, CmdEvents, Connection
 import mcpi.block as block
+import asyncio
 
 from mcpi.minecraft import Minecraft
 import mcpi.block as block
 import time
+#Dades per conectarme al servidor
+SERVER_HOST = 'localhost'
+ADDRES = 25565
 
-global mc
-mc = Minecraft.create('localhost', 25565)
 
-for event in mc.events.pollChatPosts():
-        print(event)
+mc = Minecraft.create()
+while True:
+    for i in mc.events.pollChatPosts():
+        print(i)
+ 
+   
